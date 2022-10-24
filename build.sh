9b56fe64c242be7d2381c3d100c6a558be45e3e0
floj <floj@users.noreply.github.com>
2022-10-24 10:33:31 +0200

    update modules

--- build.sh
+++ build.sh
@@ -2,8 +2,5 @@
 set -euo pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-
-caddy_version=2.4.6
-
-xcaddy build "v$caddy_version" --output "$script_dir/caddy" \
+xcaddy build --output "$script_dir/caddy" \
   --with github.com/floj/caddy-cors="$script_dir"
